30d8e65ee1d106378d0876b9f9bd4d490cfa9af0
stpCollabr8nLstn <amrios2817@gmail.com>
2017-02-08 08:13:40 +0000

    clean style

--- server.js
+++ server.js
@@ -10,22 +10,38 @@ app.use(express.static(__dirname + '/public'))
 
 // Serve homepage
 app.get('/', (req, res) => {
-  res.render('index', {title: 'Home'});
+  res.render('index', {
+    title: 'Home', 
+    showContent: false,
+    showSplash: true
+  });
 });
 
 // Serve view
 app.get('/view', (req, res) => {
-    res.render('view', {title: 'View Fish'});
+    res.render('view', {
+      title: 'View Fish',
+      showContent: true,
+      showSplash: false
+    });
 });
 
 // Serve feeder
 app.get('/feeder', (req, res) => {
-  res.render('index', {title: 'Feeder'});
+  res.render('feeder', {
+    title: 'Feeder',
+    showContent: true,
+    showSplash: false
+  });
 });
 
 // Serve about
 app.get('/about', (req, res) => {
-  res.render('index', {title: 'About Us'});
+  res.render('about', {
+    title: 'About Us',
+    showContent: true,
+    showSplash: false
+  });
 });
 
 var piREST = require('pi-arest')(app);
