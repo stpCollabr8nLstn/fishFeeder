a326d000c79c3e29b89dd7ec75cb3b5cddec655b
amrios <amrios2817@gmail.com>
2017-04-03 23:12:45 -0500

    add hours until next feeding

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,1 +1,22 @@
+function checkFeedTime() {
+  var now = new Date();
+  var curHr = now.getHours();
+  var nxtFeeding;
 
+  if ( curHr < 6 ) {
+    return 6 - curHr;
+  }
+
+  if ( curHr > 6 && curHr < 18) {
+    return 18 - curHr;
+  }
+
+  return ( 24 - curHr ) + 6;
+}
+
+function revealTime() {
+  $('li.time').removeClass('hidden');
+  $(this).addClass('active');
+  var numHrs = checkFeedTime();
+  $('p.hours').prepend(numHrs);
+}
